4abfc5982ec0f90c26be4163567205e2c7f8ce4b
Mukesh <mouryamukesh8692@gmail.com>
2023-11-11 18:02:34 +0530

    fixed dashboard

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -13,7 +13,7 @@ const Dashboard = () => {
 
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
   const { data, mutate, error, isLoading } = useSWR(
-    `https://next-blog-cuxqsqzlv-mukeshs-projects-c3577dae.vercel.app/api/posts?username=${session?.data?.user.name}`,
+    `/api/posts?username=${session?.data?.user.name}`,
     fetcher
   );
   console.log(data);
